7d81f3408ad952cf12366e07c285a0ad331f004a
NgDQuocHuy <NgDQuocHuy241@gmail.com>
2022-10-11 16:52:43 +0700

    Bai 6 Demo 1

---
+++ Bai6/demo1/src/main/java/com/codegym/demo1/repository/customerRepository/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.demo1.repository.customerRepository;
+
+import com.codegym.demo1.model.Customer;
+import com.codegym.demo1.repository.IGeneralRepository;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer>{
+}
---
+++ Bai6/demo1/src/main/java/com/codegym/demo1/service/customerService/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.codegym.demo1.service.customerService;
+
+import com.codegym.demo1.model.Customer;
+import com.codegym.demo1.repository.IGeneralRepository;
+import com.codegym.demo1.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
